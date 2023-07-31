1cef4d75ebd2317e3862b59e3c15f50680588c63
Eyu <eyobedmes2@gmail.com>
2023-07-31 16:51:28 +0300

    recent jobs updated

--- job/frontend/src/components/recentJobs/Recent.jsx
+++ job/frontend/src/components/recentJobs/Recent.jsx
@@ -3,10 +3,16 @@ import RecentJobsCard from './RecentJobsCard'
 
 const Recent = () => {
   return (
-    <div className=' z-0 '> 
+    <div className=''> 
         <h1 className='text-[#0B3812] text-[48px] font-semibold text-center  mt-40'>Recent jobs</h1>
-        <div>
-            <RecentJobsCard/>
+        <div className=' flex justify-center gap-3 ml-0 w-full flex-wrap'>
+            <RecentJobsCard jobTitle="software development" campany="google" location="adiss ababa, bole" workHour="full time" link="http://localhost:5222" expireDate="2 days left"/>
+
+            <RecentJobsCard jobTitle="software development" campany="google" location="adiss ababa, bole" workHour="full time" link="http://localhost:5222" expireDate="2 days left"/>
+
+            <RecentJobsCard jobTitle="software development" campany="google" location="adiss ababa, bole" workHour="full time" link="http://localhost:5222" expireDate="2 days left"/>
+
+            <RecentJobsCard jobTitle="software development" campany="google" location="adiss ababa, bole" workHour="full time" link="http://localhost:5222" expireDate="2 days left"/>
         </div>
     </div>
   )
--- job/frontend/src/components/recentJobs/RecentJobsCard.jsx
+++ job/frontend/src/components/recentJobs/RecentJobsCard.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import editIcon from '../../assets/edit_icon.svg'
+import clockImg from '../../assets/clock.svg'
 
-const RecentJobsCard = () => {
+const RecentJobsCard = ({jobTitle, campany , location , workHour , link , expireDate}) => {
   return (
-    <div>RecentJobsCard</div>
+    <div className='borderStyle flex flex-row gap-4 flex-start sm:max-w-[288px] md:min-w-[40vw] py-2 sm:px-1 md:px-3 backdrop-opacity-10'>
+      <div>
+        <img src={editIcon} alt="job icon" className='md:w-14 md:h-14 sm:w-[32px] sm:h-[35px]' />
+      </div>
+
+
+      <div className='flex flex-row md:gap-7  '>
+        <div>
+          <h1 className='text-[#184D47] sm:text-[14px] md:text-[18px]  font-semibold  md:w-[18rem] sm:w-[10rem] flex flex-wrap relative'>{jobTitle}</h1>
+          <p className='text-[#184D47] sm:text-[12px] md:text-[14px] lg:text-base font-medium sm:mt-[22px] md:mt-[29px]'>{campany}</p>
+          <p className='text-[#184D47] sm:text-[12px] md:text-[14px] lg:text-base font-medium'>{workHour}</p>
+          <p className='text-[#184D47] sm:text-[12px] md:text-[14px] lg:text-base font-medium'>{location}</p>
+        </div>
+
+
+        <div className='  text-end items-end  md:absolute   md:right-0 sm:mt-0 '> 
+          <a href={link}  className=' md:px-4 sm:px-1 py-1 mx-1  bg-[#00917C] text-center items-center text-white sm:text-sm md:text-base font-normal' >Details</a>
+          <div className='flex  items-center md:mt-20 sm:mt-[55px]'> 
+            <img src={clockImg} alt="clock image" className='md:mx-2 sm:mx-0 sm:hidden'  />
+            <p className='text-[#b64156] sm:text-xs md:text-base font-semiboldsm:mx-0 sm:text-center'>{expireDate}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
